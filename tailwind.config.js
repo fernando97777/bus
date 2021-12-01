7377a9037eb478f05eb0770f18166d396862c44d
fernando97777 <78433050+fernando97777@users.noreply.github.com>
2021-12-01 09:41:07 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      roboto:['Roboto'],
+      oxygen:['Oxygen']
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
